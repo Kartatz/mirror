25659b4c6e0a9e4534c1bab48db20afea1811576
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-09-02 21:19:11 -0300

    Import upstream changes from OBGGCC

--- utils.sh
+++ utils.sh
@@ -13,4 +13,17 @@ function mirror() {
 	git -C "${directory}" push --force --quiet --branches "${url}"
 	git -C "${directory}" push --force --quiet --tags "${url}"
 	
+	git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
+	git config --global user.name "github-actions[bot]"
+	
+	git branch timestamp
+	git checkout timestamp
+	
+	date > timestamp
+	git add timestamp
+	git commit -m "Initial commit"
+	
+	git push --force --quiet origin timestamp
+	
 }
+	
